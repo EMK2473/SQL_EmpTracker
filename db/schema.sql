62a519cbd5022d69929744977979595f63680673
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2023-11-20 13:26:41 -0500

    adjusted emp reference in schema

--- db/schema.sql
+++ db/schema.sql
@@ -23,9 +23,11 @@ first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
 role_id INT,
 manager_id INT,
-FOREIGN KEY (manager_id)
+FOREIGN KEY (role_id)
 REFERENCES roles(id)
-ON DELETE SET NULL
+ON DELETE SET NULL,
 FOREIGN KEY (manager_id)
-REFERENCES employee(id) 
+REFERENCES employee(id)
+ON DELETE SET NULL
 );
+
